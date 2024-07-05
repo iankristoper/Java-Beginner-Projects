2a2279360407e2bd1e7cda7ec8c94a128cf86591
iankristoper <iankristoper26@gmail.com>
2024-07-06 00:17:14 +0800

    UPDATE: initial commit

---
+++ RecipeBook/src/main/java/com/ian/recipebook/RecipeBook.java
@@ -0,0 +1,28 @@
+package com.ian.recipebook;
+
+import java.util.Scanner;
+
+
+
+
+
+class Recipe
+{
+    private String name;
+    private String ingredients;
+    private String instructions;
+    
+    
+    
+}
+
+
+
+public class RecipeBook 
+{
+
+    public static void main(String[] args) 
+    {
+        
+    }
+}
